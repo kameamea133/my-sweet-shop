e4e7dc0bb1a387bcd33d949cbb70a09d01d72fcd
washtakzo <washtakzo@gmail.com>
2022-10-28 13:15:21 +0200

    lianenko correction

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,58 +1,56 @@
-import React from 'react';
-import '../styles/Section2.css';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-
-
+import React from "react";
+import "../styles/Section2.css";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import ArrowButton from "../components/arrow-button/ArrowButton";
 
 const Section2 = () => {
   return (
-    <div className='section2'>
-      <div className='section2-mainContainer'>
-        <div className='cream'>
-        <div className='cream-block'>
-          <h1>Ice Cream</h1>
-          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of.</p>
-          <h4>22 products</h4>
+    <div className="section2">
+      <div className="section2-mainContainer">
+        <div className="cream">
+          <div className="section2__cream__info">
+            <h1>Ice Cream</h1>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of.
+            </p>
+            <h4>22 products</h4>
+          </div>
+          <ArrowButton
+            text="go to category"
+            className="section2__cream__button"
+          />
         </div>
-         <div className='cream-btnBottom'>
-          <p >go to category</p>
-          <KeyboardArrowRightIcon style={{ background: "#DF5879", color: "#fff", padding:"5px", borderRadius:"9999px"}}/>
-         </div>
-          
-        </div>
-        <div className='macarons'>
+        <div className="macarons">
           <h4>15 products</h4>
+          <h2>Macaroons</h2>
         </div>
-        <div className='discounts'>
+        <div className="discounts">
+          <div />
           <h1>discounts</h1>
         </div>
-        <div className='donuts'>
+        <div className="donuts">
           <h4>18 products</h4>
           <h1>Donuts</h1>
-          <div className='donutsBtn'>
-          <p>go to category</p>
-          <KeyboardArrowRightIcon style={{ color: "#fff", background: "#CF4567", padding:"5px", borderRadius:"9999px"}}/>
-          </div>
-          
+          <ArrowButton text="go to category" />
         </div>
-        <div className='categories'>
+        <div className="categories">
           <h1>categories</h1>
         </div>
-        <div className='cheskey'>
-            <h1>Cheskey</h1>
+        <div className="cheskey">
+          <div>
             <h4>22 products</h4>
-          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots.</p>
-          <div className='cheskeyBtn'>
-            <p>go to category</p>
-          
-            <KeyboardArrowRightIcon style={{ background: "#36A6CC", padding:"5px", borderRadius:"9999px", color: "#fff"}}/>
           </div>
-          
-          
+          <h1>Cheskey</h1>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots.
+          </p>
+          <ArrowButton text="go to category" className="cheskey__arrowBtn" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Section2
+export default Section2;
