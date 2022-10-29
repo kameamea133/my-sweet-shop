a41263c0f74e08fe607d479183f6a91f6d77ec6c
washtakzo <washtakzo@gmail.com>
2022-10-29 08:51:43 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import "./App.css";
-import React from 'react'
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./helper/theme";
+import Products from "./pages/products/Products";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+]);
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </ThemeProvider>
-
   );
 }
 
